980e04f85f3c9e32fc2355870e7de0fff0052866
Adam Markowski <gobolabxd@gmail.com>
2025-01-17 23:36:18 +0100

    xx

--- src/components/ScrollBtn.tsx
+++ src/components/ScrollBtn.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { FaArrowDown } from "react-icons/fa6";
 const ScrollBtn = () => {
   return (
-    <div>
-      <div className="px-8 py-4 items-center text-[18px] gap-2 mb-20 absolute bottom-0 flex rounded-full text-white bg-white/30 backdrop-blur-md w-fit h-fit">
+    <div className="w-fit  ">
+      <div className="px-8 py-4 mb-[4vh]  items-center text-[18px] gap-2 flex rounded-full text-white bg-white/30 backdrop-blur-md w-fit h-fit">
         <FaArrowDown />
         <div>Scroll Down</div>
       </div>
@@ -12,3 +12,18 @@ const ScrollBtn = () => {
 };
 
 export default ScrollBtn;
+
+{/* <div className="flex gap-8">
+                <div className="w-fit">
+                  <div>Kontakt: </div>
+                  <div>512 321 664</div>
+                </div>
+                <div className="w-fit">
+                  <div>Prowadzi</div>
+                  <div>Ela Jakubowska</div>
+                </div>
+                <div className="w-fit">
+                  <div>Miejsce:</div>
+                  <div>Chojnice, ul. 31 Stycznia</div>
+                </div>
+              </div> */}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,24 +5,27 @@ import Cursor from "./cursor";
 import Image from "next/image";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
 const Header = () => {
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
-    gsap.set(".tracker", {
-      scale: 1,
-
-      ease: "power2.inOut",
-      scrollTrigger: {
-        trigger: ".tracker",
-        start: "top top",
-        end: "bottom top",
-        scrub: true,
+    gsap.set(
+      ".tracker",
+      {
+        scale: 1,
+        ease: "power2.inOut",
+        scrollTrigger: {
+          trigger: ".tracker",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
       },
-    });
+      []
+    );
     gsap.to(".tracker", {
       scale: 1.2,
       filter: "blur(20px)",
-
       ease: "power2.inOut",
       scrollTrigger: {
         trigger: ".tracker",
@@ -32,32 +35,30 @@ const Header = () => {
       },
     });
   });
-  return (
-    <div className="  h-screen relative overflow-hidden text-white ">
-      <Cursor></Cursor>
-      <div className="flex">
-        <div className="bottom-0 absolute mx-[1.5vw] z-[100]">
-          <div className="text-[20vw] text-center md:text-left sm:text-[9vw]   font-semibold">
-            ARTLAJK
-          </div>
-          <div className="text-[6vw] md:mb-[16vh] text-center md:text-left mb-[25vh] sm:text-[5vw] md:text-[4vw] lg:text-[3vw] xl:text-[2.5vw] mt-[-2vw] ">
-            ZAJĘCIA Z RYSUNKU I MALARSTWA
-          </div>
 
-          <ScrollBtn></ScrollBtn>
-        </div>
-        <div className="md:w-[30vw] absolute bottom-0 mb-[6vh] md:flex gap-[2vw] justify-end right-0 m-4 hidden sm:block">
-          <div className="w-fit">
-            <div>Kontakt: </div>
-            <div>512 321 664</div>
-          </div>
-          <div className="w-fit">
-            <div>Prowadzi</div>
-            <div>Ela Jakubowska</div>
-          </div>
-          <div className="w-fit">
-            <div>Miejsce:</div>
-            <div>Chojnice, ul. 31 Stycznia</div>
+  return (
+    <div className="h-screen relative flex justify-center overflow-hidden text-white">
+      <Cursor />
+      <div className="w-[96vw] relative">
+        <div className="w-full bottom-0 absolute text-center md:text-left">
+          <div className="text-[10vh] md:text-[20vh] leading-none">ARTLAJK</div>
+          <div className="text-[4vh] mb-[5vh] md:text-[6vh]">ZAJĘCIA Z MALATSTWA I RYSNKU</div>
+          <div className="flex flex-col gap-[10vh] md:flex-row w-full justify-center items-center md:justify-between">
+            <ScrollBtn />
+            <div className="flex text-left gap-8 md:text-right">
+              <div className="w-fit">
+                <div>Kontakt</div>
+                <div>512 321 664</div>
+              </div>
+              <div className="w-fit text-center md:text-right">
+                <div>Prowadzi</div>
+                <div>Ela Jakubowska</div>
+              </div>
+              <div className="w-fit  text-right md:text-right">
+                <div>Miejsce</div>
+                <div>Chojnice, ul. 31 Stycznia</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -67,7 +68,7 @@ const Header = () => {
         height={2000}
         className="tracker absolute overflow-hidden w-full h-screen object-cover z-[-192]"
         src={"/img/bgimg.png"}
-      ></Image>
+      />
     </div>
   );
 };
