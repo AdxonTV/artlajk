bd67e7bdbae98c2add151c6a02e44597c14d1ff9
Adam Markowski <gobolabxd@gmail.com>
2025-01-24 16:43:39 +0100

    mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,31 +9,34 @@ const Malownia = lazy(() => import("@/components/Malownia"));
 
 export default function Home() {
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: "vertical",
-      gestureOrientation: "vertical",
-      smoothWheel: true,
-      touchMultiplier: 2,
-      infinite: false,
-    });
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-    function raf(time: number) {
-      lenis.raf(time);
+    if (!isMobile) {
+      const lenis = new Lenis({
+        duration: 1.2,
+        easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        orientation: "vertical",
+        gestureOrientation: "vertical",
+        smoothWheel: true,
+        touchMultiplier: 2,
+        infinite: false,
+      });
+
+      function raf(time: number) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
       requestAnimationFrame(raf);
     }
-    requestAnimationFrame(raf);
   }, []);
 
   return (
     <div className="overflow-x-hidden">
-      <Suspense fallback={<Loading/>}>
+      <Suspense fallback={<Loading />}>
         <Navbar />
         <Header />
         <div className="h-[20vh]"></div>
         <Malownia />
-  
       </Suspense>
     </div>
   );
