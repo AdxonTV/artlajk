7c6fd0a1d1f7c08c052a0b11967a49fa1a8e7bd6
Adam Markowski <gobolabxd@gmail.com>
2025-01-24 13:59:29 +0100

    da

--- src/components/Malownia.tsx
+++ src/components/Malownia.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MalowniaHeader from './MalowniaHeader'
 import MalowniaWorks from './MalowniaWorks'
-import HorizontalScroll from './HorizontalScroll'
+
 const Malownia = () => {
   return (
     <div className='flex gap-[30vh] my-[2vh] md:gap-[5vh] relative flex-col'>
