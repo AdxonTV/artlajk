eddfbd04609580d99468596fe16250ce0a49ca62
Adam Markowski <gobolabxd@gmail.com>
2025-01-31 19:31:44 +0100

    ddd

--- src/components/Ss.tsx
+++ src/components/Ss.tsx
@@ -1,29 +1,33 @@
-import { useEffect, useRef } from "react";
+"use client";
+import { useEffect, useRef, useState } from "react";
 import React from "react";
 
 const Ss = () => {
   const pathRef = useRef<SVGPathElement>(null);
-const isMobile = window.innerWidth < 768;
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (pathRef.current) {
-        const pathLength = pathRef.current.getTotalLength();
-        const scrollPercentage =
-          window.scrollY / (document.body.scrollHeight - window.innerHeight);
-        const drawLength = pathLength * scrollPercentage * 6.5;
+    if (typeof window !== "undefined") {
+      setIsMobile(window.innerWidth < 768);
 
-        pathRef.current.style.strokeDasharray = `${drawLength} ${pathLength}`;
-      }
-    };
+      const handleScroll = () => {
+        if (pathRef.current) {
+          const pathLength = pathRef.current.getTotalLength();
+          const scrollPercentage =
+            window.scrollY / (document.body.scrollHeight - window.innerHeight);
+          const drawLength = pathLength * scrollPercentage * 6.5;
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
+          pathRef.current.style.strokeDasharray = `${drawLength} ${pathLength}`;
+        }
+      };
+
+      window.addEventListener("scroll", handleScroll);
+      return () => window.removeEventListener("scroll", handleScroll);
+    }
   }, []);
 
   return (
     <div>
-  
       <svg
         width="950"
         height="1193"
@@ -33,19 +37,18 @@ const isMobile = window.innerWidth < 768;
       >
         <defs>
           <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
-        <stop offset="0%" style={{ stopColor: "#5B4CFF", stopOpacity: 1 }} />
-        <stop offset="100%" style={{ stopColor: "#863AF1", stopOpacity: 1 }} />
+            <stop offset="0%" style={{ stopColor: "#5B4CFF", stopOpacity: 1 }} />
+            <stop offset="100%" style={{ stopColor: "#863AF1", stopOpacity: 1 }} />
           </linearGradient>
         </defs>
         <path
           ref={pathRef}
           d="M19.4982 17.4986C1367.01 1453 893.504 -603 310 1184.5"
           stroke="url(#gradient)"
-          strokeWidth={isMobile ? 14 : 52 }
+          strokeWidth={isMobile ? 14 : 52}
           fill="transparent"
         />
       </svg>
-    
     </div>
   );
 };
