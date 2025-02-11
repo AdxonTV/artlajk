4ac20cab8777e21cae95da9e29b0bb5449f25718
Adam Markowski <gobolabxd@gmail.com>
2025-02-11 19:12:05 +0100

    ll

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,21 +1,6 @@
-"use client"
-import React, { useState, useEffect } from "react";
-import SsAuto from "@/components/SsAuto";
 
 const Loading = () => {
-  const [visible, setVisible] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVisible(false);
-    }, 10000); // 10 seconds
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  if (!visible) {
-    return null;
-  }
 
   return (
     <div className="w-full h-[100vh] text-black flex justify-center items-center text-[100px] opacity-30 ">
